9aca4e6ce64474c994f4b41fc3cc7e55d9b085d5
Ka Wing Tsang <kawingt2@gmail.com>
2021-03-16 09:49:42 +0800

    Set up API call to get weather info

---
+++ weather.js
@@ -0,0 +1,21 @@
+class Weather {
+    constructor(city, state, country) {
+        this.apiKey = '64bbc6074df6622114643520067a3825';
+        this.city = city;
+        this.state = state;
+        this.country = country;
+    }
+    // Fetch weather from API
+    async getWeather() {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state},${this.country}&appid=${this.apiKey}`);
+        const responseData = await response.json();
+
+        return responseData
+    }
+    // change weather location
+    changeLocation(city, state, country) {
+        this.city = city;
+        this.state = state;
+        this.country = country
+    }
+}
